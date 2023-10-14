612169323a349ca03cf8fbf4b05026304ff85317
Furkan Vural <furkanvural52@gmail.com>
2023-10-14 23:07:44 +0300

    Create app folder

---
+++ CodeBuddy/CodeBuddy/CodeBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBuddyApp.swift
+//  CodeBuddy
+//
+//  Created by furkan vural on 14.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CodeBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
