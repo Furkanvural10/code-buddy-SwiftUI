f28874299c4bdfe909160074e71e7b2c14e76f20
Furkan Vural <furkanvural52@gmail.com>
2023-10-20 23:37:25 +0300

    Add homeview page

---
+++ CodeBuddy/CodeBuddy/Screens/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  CodeBuddy
+//
+//  Created by furkan vural on 20.10.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
