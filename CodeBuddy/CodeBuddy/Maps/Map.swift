1ecd22f08df33ef38177b78df518c3b10653c8b6
Furkan Vural <furkanvural52@gmail.com>
2023-10-21 00:18:59 +0300

    Setup map view

---
+++ CodeBuddy/CodeBuddy/Maps/Map.swift
@@ -0,0 +1,26 @@
+//
+//  Map.swift
+//  CodeBuddy
+//
+//  Created by furkan vural on 20.10.2023.
+//
+
+import Foundation
+import MapKit
+import SwiftUI
+
+struct Map: NSViewRepresentable {
+    typealias NSViewType = MKMapView
+    
+    func makeNSView(context: Context) -> MKMapView {
+        let map = MKMapView()
+        map.showsUserLocation = true
+        return map
+    }
+    
+    func updateNSView(_ nsView: MKMapView, context: Context) {
+        
+    }
+    
+    
+}
--- CodeBuddy/CodeBuddy/Screens/MapView.swift
+++ CodeBuddy/CodeBuddy/Screens/MapView.swift
@@ -1,15 +1,9 @@
-//
-//  MapView.swift
-//  CodeBuddy
-//
-//  Created by furkan vural on 20.10.2023.
-//
 
 import SwiftUI
 
 struct MapView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map()
     }
 }
 
