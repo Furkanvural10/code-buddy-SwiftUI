0ce34bd017a6b51e6bdece7eb0a0e5c8c0682ce4
Furkan Vural <furkanvural52@gmail.com>
2023-10-26 01:41:13 +0300

    Add opacity adding pin view rectangle

--- CodeBuddy/CodeBuddy/Screens/MapView.swift
+++ CodeBuddy/CodeBuddy/Screens/MapView.swift
@@ -3,7 +3,19 @@ import SwiftUI
 
 struct MapView: View {
     var body: some View {
-        Map()
+        
+        ZStack(alignment: .centerLastTextBaseline) {
+            
+            Map()
+            Rectangle()
+                .opacity(0.1)
+                .frame(width: 300, height: 70)
+                .cornerRadius(5)
+                .padding(.bottom, 20)
+                
+                
+                
+        }
     }
 }
 
