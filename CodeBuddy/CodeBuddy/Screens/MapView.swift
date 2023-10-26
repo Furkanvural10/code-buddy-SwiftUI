bb8f7aa1f5c6ff5eba77f8e609257c3b92aeb376
Furkan Vural <furkanvural52@gmail.com>
2023-10-26 12:30:33 +0300

    Add pin view and related icons

--- CodeBuddy/CodeBuddy/Screens/MapView.swift
+++ CodeBuddy/CodeBuddy/Screens/MapView.swift
@@ -4,17 +4,40 @@ import SwiftUI
 struct MapView: View {
     var body: some View {
         
+        
         ZStack(alignment: .centerLastTextBaseline) {
-            
             Map()
+            
             Rectangle()
-                .opacity(0.1)
+                .fill(.black.opacity(0.31))
                 .frame(width: 300, height: 70)
-                .cornerRadius(5)
-                .padding(.bottom, 20)
-                
-                
-                
+                .cornerRadius(10)
+                .overlay(
+                    HStack(alignment: .center, spacing: 40) {
+                        Image(systemName: "plus.circle.fill") // İlk simge için sisteminizdeki simgenin ismini ekleyin
+                            .font(.system(size: 24))
+                            .foregroundColor(.white)
+                        Image(systemName: "star.fill") // İkinci simge için sisteminizdeki simgenin ismini ekleyin
+                            .font(.system(size: 24))
+                            .foregroundColor(.yellow)
+                        Image(systemName: "square.stack.3d.up.fill") // Üçüncü simge için sisteminizdeki simgenin ismini ekleyin
+                            .font(.system(size: 24))
+                            .foregroundColor(.green)
+                        Image(systemName: "location.fill") // Üçüncü simge için sisteminizdeki simgenin ismini ekleyin
+                            .font(.system(size: 24))
+                            .foregroundColor(.blue)
+                    }
+                )
+                .padding(.bottom, 25)
+            
+            
+            
+            
+            
+            
+            
+            
+            
         }
     }
 }
@@ -24,3 +47,5 @@ struct MapView_Previews: PreviewProvider {
         MapView()
     }
 }
+
+
