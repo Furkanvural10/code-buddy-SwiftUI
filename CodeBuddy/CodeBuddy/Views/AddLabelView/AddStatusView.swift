20f6df621e608ed04c1448dc1108696db9046afa
Furkan Vural <furkanvural52@gmail.com>
2023-11-01 23:43:41 +0300

    Add custom toggleButtonStyle struct

--- CodeBuddy/CodeBuddy/Views/AddLabelView/AddStatusView.swift
+++ CodeBuddy/CodeBuddy/Views/AddLabelView/AddStatusView.swift
@@ -62,19 +62,20 @@ struct AddStatusView: View {
                             .font(.system(size: 11))
                             .foregroundColor(onHoverForCollaborationStatus ? Color.gray : Color.white.opacity(0))
                             .offset(x: 0, y: -22)
-                        Toggle(isOn: $isCollaborationStatus) {
-                            Image(systemName: "keyboard.chevron.compact.left.fill")
-                                .foregroundColor(isCollaborationStatus ? Color.green : Color.gray)
-                                .font(.system(size: 20))
-                        }
-                        
-                        
-                        
+//                        Toggle(isOn: $isCollaborationStatus) {
+//                            Image(systemName: "keyboard.chevron.compact.left.fill")
+//                                .foregroundColor(isCollaborationStatus ? Color.green : Color.gray)
+//                                .font(.system(size: 20))
+//                        }
+                        Toggle("", isOn: $isCollaborationStatus)
+                            .toggleStyle(MyCustomToggleStyle())
                     }
-                    
                     .onHover { hovering in
                         self.onHoverForCollaborationStatus = hovering
                     }
+                    
+                    
+                        
                     
                     
                     
@@ -148,4 +149,34 @@ struct AddStatusView_Previews: PreviewProvider {
     static var previews: some View {
         AddStatusView()
     }
+}
+
+
+struct MyCustomToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+            HStack {
+                configuration.label
+                Spacer()
+                Rectangle()
+                    .foregroundColor(configuration.isOn ? .green : .gray)
+                    .frame(width: 51, height: 31, alignment: .center)
+                    .overlay(
+                        Circle()
+                            .foregroundColor(.white)
+                            .padding(.all, 3)
+                            .overlay(
+                                Image(systemName: configuration.isOn ? "checkmark" : "xmark")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .font(Font.title.weight(.black))
+                                    .frame(width: 8, height: 8, alignment: .center)
+                                    .foregroundColor(configuration.isOn ? .green : .gray)
+                            )
+                            .offset(x: configuration.isOn ? 11 : -11, y: 0)
+                            .animation(Animation.linear(duration: 0.1))
+                            
+                    ).cornerRadius(20)
+                    .onTapGesture { configuration.isOn.toggle() }
+            }
+        }
 }
