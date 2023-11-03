5920eb7de194f20180681749f7df465f43252094
Furkan Vural <furkanvural52@gmail.com>
2023-11-03 15:36:47 +0300

    Add status toggle style and fixed its color

--- CodeBuddy/CodeBuddy/Views/AddLabelView/AddStatusView.swift
+++ CodeBuddy/CodeBuddy/Views/AddLabelView/AddStatusView.swift
@@ -25,6 +25,7 @@ struct AddStatusView: View {
     
     var body: some View {
         VStack(alignment: .trailing) {
+            Spacer()
             HStack {
                 ZStack {
                     Image(systemName: "square.and.arrow.down")
@@ -39,36 +40,32 @@ struct AddStatusView: View {
                     Text("Add your image")
                         .font(.system(size: 10))
                         .foregroundColor(onHoverForImage ? Color.gray : Color.white.opacity(0))
-                        .offset(x: 0, y: -44)
+                        .offset(x: 0, y: 40)
                 }
                     
                 VStack {
                     TextField("Username", text: $nameText)
                     TextField("Backend Developer", text: $titleText)
                 }
+            
                 .padding(.trailing, 10)
             }
             
             HStack {
-                Text("Select Mood:")
-                    .font(.system(size: 12))
-                    .foregroundColor(.white.opacity(0.5))
-                
-                HStack {
                     ZStack {
                         // MARK: - Collaboration Status
-                        Text("Collaboration")
+                        Text("")
                             .scaledToFill()
                             .font(.system(size: 11))
                             .foregroundColor(onHoverForCollaborationStatus ? Color.gray : Color.white.opacity(0))
-                            .offset(x: 0, y: -22)
+                            .offset(x: 40, y: -28)
 //                        Toggle(isOn: $isCollaborationStatus) {
 //                            Image(systemName: "keyboard.chevron.compact.left.fill")
 //                                .foregroundColor(isCollaborationStatus ? Color.green : Color.gray)
 //                                .font(.system(size: 20))
 //                        }
-                        Toggle("", isOn: $isCollaborationStatus)
-                            .toggleStyle(MyCustomToggleStyle())
+                        Toggle("Collaboration", isOn: $isCollaborationStatus)
+                            .toggleStyle(MyCollaborationCustomToggleStyle())
                     }
                     .onHover { hovering in
                         self.onHoverForCollaborationStatus = hovering
@@ -81,17 +78,19 @@ struct AddStatusView: View {
                     
                     ZStack {
                         // MARK: - Networking Status
-                        Text("Networking")
+                        Text("")
                             .scaledToFill()
                             .font(.system(size: 11))
                             .foregroundColor(onHoverForNetworkingStatus ? Color.gray : Color.white.opacity(0))
                             .offset(x: 0, y: -22)
-                        Toggle(isOn: $isNetworkingStatus) {
-                            Image(systemName: "cup.and.saucer.fill")
-                                .foregroundColor(isNetworkingStatus ? Color.brown : Color.gray)
-                                .font(.system(size: 20))
-                                
-                        }
+                        Toggle("Networking", isOn: $isNetworkingStatus)
+                            .toggleStyle(MyNetworkingCustomToggleStyle())
+//                        Toggle(isOn: $isNetworkingStatus) {
+//                            Image(systemName: "cup.and.saucer.fill")
+//                                .foregroundColor(isNetworkingStatus ? Color.brown : Color.gray)
+//                                .font(.system(size: 20))
+//
+//                        }
                         
                     }
                     .onHover { hovering in
@@ -100,30 +99,33 @@ struct AddStatusView: View {
                     
                     // MARK: - Busy Status
                     ZStack {
-                        Text("Busy")
+                        Text("")
                             .scaledToFill()
                             .font(.system(size: 11))
                             .foregroundColor(onHoverForBusyStatus ? Color.gray : Color.white.opacity(0))
                             .offset(x: 0, y: -22)
-                        Toggle(isOn: $isBusyStatus) {
-                            Image(systemName: "minus.circle.fill")
-                                .foregroundColor(isBusyStatus ? Color.red : Color.gray)
-                                .font(.system(size: 20))
-                        }
-                        .onTapGesture {
-                            print("Tıklandı Busy")
-                            isNetworkingStatus = false
-                            isCollaborationStatus = false
-                        }
+                        Toggle("Busy", isOn: $isBusyStatus)
+                            .toggleStyle(MyBusyCustomToggleStyle())
+//                        Toggle(isOn: $isBusyStatus) {
+//                            Image(systemName: "minus.circle.fill")
+//                                .foregroundColor(isBusyStatus ? Color.red : Color.gray)
+//                                .font(.system(size: 20))
+//                        }
+//                        .onTapGesture {
+//                            print("Tıklandı Busy")
+//                            isNetworkingStatus = false
+//                            isCollaborationStatus = false
+//                        }
                     }
                     .onHover { hovering in
                         self.onHoverForBusyStatus = hovering
                     }
-                }
+                
             }
             .padding([.trailing, .leading], 23)
             Divider()
                 .background(.white.opacity(0))
+                .padding(.top, 10)
             HStack(spacing: 10) {
                 Button("Cancel") {
                     
@@ -136,11 +138,11 @@ struct AddStatusView: View {
                 .buttonStyle(.borderedProminent)
             }
             .padding()
-            
+            Spacer()
         
             
         }
-        .frame(width: 400, height: 220)
+        .frame(width: 460, height: 210)
  
     }
 }
@@ -152,25 +154,79 @@ struct AddStatusView_Previews: PreviewProvider {
 }
 
 
-struct MyCustomToggleStyle: ToggleStyle {
+struct MyNetworkingCustomToggleStyle: ToggleStyle {
     func makeBody(configuration: Configuration) -> some View {
-            HStack {
+            VStack {
                 configuration.label
-                Spacer()
                 Rectangle()
-                    .foregroundColor(configuration.isOn ? .green : .gray)
-                    .frame(width: 51, height: 31, alignment: .center)
+                    .foregroundColor(configuration.isOn ? .brown : .gray)
+                    .frame(width: 54, height: 30, alignment: .center)
                     .overlay(
                         Circle()
-                            .foregroundColor(.white)
+                            .foregroundColor(configuration.isOn ? .white : .white)
                             .padding(.all, 3)
                             .overlay(
-                                Image(systemName: configuration.isOn ? "checkmark" : "xmark")
+                                Image(systemName: configuration.isOn ? "cup.and.saucer.fill" : "cup.and.saucer.fill")
                                     .resizable()
                                     .aspectRatio(contentMode: .fit)
-                                    .font(Font.title.weight(.black))
-                                    .frame(width: 8, height: 8, alignment: .center)
+                                    .font(Font.title.weight(.light))
+                                    .frame(width: 20, height: 20, alignment: .center)
+                                    .foregroundColor(configuration.isOn ? .brown : .gray)
+                            )
+                            .offset(x: configuration.isOn ? 11 : -11, y: 0)
+                            .animation(Animation.linear(duration: 0.1))
+                    ).cornerRadius(20)
+                    .onTapGesture { configuration.isOn.toggle() }
+            }
+        }
+}
+
+struct MyCollaborationCustomToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+            VStack {
+                configuration.label
+                Rectangle()
+                    .foregroundColor(configuration.isOn ? .green : .gray)
+                    .frame(width: 54, height: 30, alignment: .center)
+                    .overlay(
+                        Circle()
+                            .foregroundColor(configuration.isOn ? .white : .white)
+                            .padding(.all, 3)
+                            .overlay(
+                                Image(systemName: configuration.isOn ? "keyboard.fill" : "keyboard.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .font(Font.title.weight(.light))
+                                    .frame(width: 20, height: 20, alignment: .center)
                                     .foregroundColor(configuration.isOn ? .green : .gray)
+                            )
+                            .offset(x: configuration.isOn ? 11 : -11, y: 0)
+                            .animation(Animation.linear(duration: 0.1))
+                            
+                    ).cornerRadius(20)
+                    .onTapGesture { configuration.isOn.toggle() }
+            }
+        }
+}
+
+struct MyBusyCustomToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+            VStack {
+                configuration.label
+                Rectangle()
+                    .foregroundColor(configuration.isOn ? .red : .gray)
+                    .frame(width: 54, height: 30, alignment: .center)
+                    .overlay(
+                        Circle()
+                            .foregroundColor(configuration.isOn ? .white : .white)
+                            .padding(.all, 3)
+                            .overlay(
+                                Image(systemName: configuration.isOn ? "minus.circle" : "minus.circle")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .font(Font.title.weight(.light))
+                                    .frame(width: 20, height: 20, alignment: .center)
+                                    .foregroundColor(configuration.isOn ? .red : .gray)
                             )
                             .offset(x: configuration.isOn ? 11 : -11, y: 0)
                             .animation(Animation.linear(duration: 0.1))
